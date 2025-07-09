6bf3d6b50326d7e0d529929052752614580bd8c6
laxxxx <lakshmancool9@gmail.com>
2025-07-09 23:31:39 +0530

    initial commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Sree Lakshman on 09/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
